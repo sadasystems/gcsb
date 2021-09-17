1d86124b0424002e4e5cf1e4097d846526ee9b82
Brandon <lordnynex@gmail.com>
2021-09-17 14:00:35 -0700

    Add better config struct and testing

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,37 @@
 package config
 
-type (
-	Config struct{}
+import (
+	"github.com/hashicorp/go-multierror"
+	"github.com/spf13/viper"
 )
+
+// Assert that Config implements Validate
+var _ Validate = (*Config)(nil)
+
+// Set Configuration Defaults
+func SetDefaults(v *viper.Viper) {
+	// Default Connection
+	v.SetDefault("connection.num_conns", 10)
+	// v.SetDefault("")
+}
+
+type (
+	Validate interface {
+		Validate() error
+	}
+	Config struct {
+		Connection Connection `mapstructure:"connection"`
+	}
+)
+
+func (c *Config) Validate() error {
+	var result *multierror.Error
+
+	// Validate connection block
+	errs := c.Connection.Validate()
+	if errs != nil {
+		result = multierror.Append(result, errs)
+	}
+
+	return result.ErrorOrNil()
+}
