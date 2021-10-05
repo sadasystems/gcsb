1cf48d5a962821c0a0ba1a1a85a579edda25ce9d
Brandon <lordnynex@gmail.com>
2021-10-05 16:13:04 -0700

    parse spanner type

--- pkg/schema/column.go
+++ pkg/schema/column.go
@@ -2,10 +2,17 @@ package schema
 
 import (
 	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"cloud.google.com/go/spanner"
+	"cloud.google.com/go/spanner/spansql"
 	"github.com/sadasystems/gcsb/pkg/schema/information"
 )
+
+var lengthRegexp = regexp.MustCompile(`\(([0-9]+|MAX)\)$`)
 
 type (
 	Column interface {
@@ -27,6 +34,7 @@ type (
 		SpannerState() string
 		SetPrimaryKey(bool)
 		PrimaryKey() bool
+		Type() spansql.Type
 	}
 
 	column struct {
@@ -161,4 +169,62 @@ func (c *column) SetPrimaryKey(x bool) {
 
 func (c *column) PrimaryKey() bool {
 	return c.primaryKey
+}
+
+// Parse the sql type and wrap it with spansql.Type. Parts of this function borrowed from Yo
+func (c *column) Type() spansql.Type {
+	ret := spansql.Type{}
+
+	dt := c.spannerType
+
+	// separate type and length from dt with length such as STRING(32) or BYTES(256)
+	m := lengthRegexp.FindStringSubmatchIndex(dt)
+	if m != nil {
+		lengthStr := dt[m[2]:m[3]]
+		if lengthStr == "MAX" {
+			ret.Len = spansql.MaxLen
+		} else {
+			l, err := strconv.Atoi(lengthStr)
+			if err != nil {
+				panic("could not convert precision")
+			}
+			ret.Len = int64(l)
+		}
+
+		// trim length from dt
+		dt = dt[:m[0]] + dt[m[1]:]
+	}
+
+	if strings.HasPrefix(dt, "ARRAY<") {
+		ret.Array = true
+		dt = strings.TrimSuffix(strings.TrimPrefix(dt, "ARRAY<"), ">")
+	}
+
+	ret.Base = parseType(dt)
+
+	return ret
+}
+
+func parseType(dt string) spansql.TypeBase {
+	var ret spansql.TypeBase
+	switch dt {
+	case "BOOL":
+		ret = spansql.Bool
+	case "STRING":
+		ret = spansql.String
+	case "INT64":
+		ret = spansql.Int64
+	case "FLOAT64":
+		ret = spansql.Float64
+	case "BYTES":
+		ret = spansql.Bytes
+	case "TIMESTAMP":
+		ret = spansql.Timestamp
+	case "DATE":
+		ret = spansql.Date
+	default:
+		panic(fmt.Sprintf("unknown spanner type '%s'", dt))
+	}
+
+	return ret
 }
